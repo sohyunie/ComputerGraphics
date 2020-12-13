c87b1a445e08217f194634bfbe8059add9491f3b
uiwns811 <dltnals5809@gmail.com>
2020-12-14 05:21:44 +0900

    음..마우스 입력 이거 커밋되나요 나 지금 오류나서 폴더를 바꿈 ㅠㅠㅠ

---
+++ vertex.glsl
@@ -0,0 +1,24 @@
+#version 330
+
+layout (location = 0)in vec3 in_Position;
+layout (location = 1)in vec3 vNormal;
+//layout (location = 2)in vec2 vTexCoord;
+
+uniform mat4 Transform;
+uniform mat4 View;
+uniform mat4 Projection;
+uniform vec3 in_Color;
+
+out vec3 ex_Color;
+//out vec2 TexCoord;
+out vec3 FragPos;
+out vec3 Normal;
+
+void main()
+{
+	gl_Position = Projection * View * Transform * vec4 (in_Position.x, in_Position.y, in_Position.z, 1.0);
+	ex_Color = in_Color;
+	// TexCoord = vTexCoord;
+	FragPos = vec3(Transform * vec4(in_Position, 1.0));
+	Normal = vNormal;
+}
